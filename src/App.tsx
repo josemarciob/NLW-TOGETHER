7a602b70cb152c44e9c53af4094a2263bbe9eb1b
José Marcio <jjmarcio159@gmail.com>
2021-06-23 14:22:51 -0300

    D02

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './context/AuthContext';
+
 function App() {
   return (
-    <h1>Hello Worldd</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
